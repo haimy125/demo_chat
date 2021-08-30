cd855e48ff0c1d724798e9eec895feb98d50a1b8
haimy125 <taobuonquama143@gmail.com>
2021-08-30 21:39:06 +0700

    updateee

---
+++ .history/NODEJS_BASIC/demo1_20210830135640.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var dt = require('./module_data');
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+}).listen(8080);
---
+++ .history/NODEJS_BASIC/demo1_20210830135653.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var dt = require('./module_data');
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end('Hello World!');
+}).listen(8080);
